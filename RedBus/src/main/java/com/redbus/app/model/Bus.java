60bdaa58bedbdca4f3c529af5cea701c89c69eb8
addkulkarni <addkulkarni@gmail.com>
2024-07-10 14:13:26 +0530

    Commit 2

--- RedBus/src/main/java/com/redbus/app/model/Bus.java
+++ RedBus/src/main/java/com/redbus/app/model/Bus.java
@@ -4,20 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import lombok.Data;
 
 @Data
 @Entity
+@IdClass(BusId.class)
 public class Bus
 {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int bookingid;
 	private int busid;
+	@Id
 	private String busName;
 	private String fromLoc;
 	private String toLoc;
 	private String date;
 	private String time;
 	private int price;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int bookingid;
 }
